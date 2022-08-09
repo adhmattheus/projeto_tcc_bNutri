55ff735c5fbcd0a85176b1529ecd0d07f4e6f4be
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-08-08 21:22:54 -0300

    fotos alimentos

--- lib/modules/listas/passosList.dart
+++ lib/modules/listas/passosList.dart
@@ -8,49 +8,49 @@ final List<CardItem> passoslist = [
       titulo: "Passo 1",
       info:
           "Amamentar até 2 anos ou mais, oferencendo somente o leite materno até 6 meses.",
-      image: "assets/imgs/mars.png",
+      image: "assets/imgs/ps1.png",
       texto:
           "O leite materno é muito importante para a criança até 2 anos ou mais, sendo o único alimento que a criança deve receber até 6 meses, sem necessidade de água, chá ou qualquer outro alimento. Começar a amamentação logo após o nascimento, na primeira hora de vida, traz benefícios para a criança e para a mãe, além disso, a existência de uma rede de apoio à mãe que amamenta é importante para o sucesso da amamentação."),
   const CardItem(
       id: "2",
       titulo: "Passo 2",
       info: "Oferecer alimentos, além do leite materno, a partir dos 6 meses.",
-      image: "assets/imgs/moon.png",
+      image: "assets/imgs/ps2.png",
       texto:
           "O consumo de outros alimentos IN NATURA ou minimamente processados além do leite materno passa a ser necessário para o pleno crescimento e desenvolvimento da criança a partir dos 6 meses. A alimentação da criança deve ser composta por comida de verdade, isto é, refeições feitas com alimentos in natura ou minimamente processados de diferentes grupos (por exemplo, feijões, cereais, raízes e tubérculos, frutas, legumes e verduras, carnes). Sempre que puder, varie a oferta de alimentos ao longo do dia e ao longo da semana."),
   const CardItem(
       id: "3",
       titulo: "Passo 3",
       info: "Oferecer água pra o consumo à criança.",
-      image: "assets/imgs/mercury.png",
+      image: "assets/imgs/ps3.png",
       texto:
           "Oferecer água própria pra o consumo à criança em vez de sucos, refrigerantes e outras bebidas açucaradas. O acesso à água potável para beber e para produção de refeições é também um direito humano.  Habituar a criança a ingerir essas bebidas açucaradas aumenta a chance de a criança apresentar excesso de peso e cárie dentária, além de desestimular o consumo de água. Ofereça frequentemente água para a criança, entre as refeições, mesmo sem ela pedir."),
   const CardItem(
       id: "4",
       titulo: "Passo 4",
       info: "Oferecer comida amassada para criança",
-      image: "assets/imgs/mercury.png",
+      image: "assets/imgs/ps4.png",
       texto:
           "Oferecer a comida amassada quando a criança começar a comer outros alimentos além do leite materno. A comida com consistência adequada à criança contribui para seu desenvolvimento, além de conter, em geral, mais energia e nutrientes do que refeições mais líquidas. A mastigação estimula o desenvolvimento da face e dos ossos da cabeça. Desde o início o alimento deve ser espesso o suficiente para não “escorrer” da colher. No início, amassar os alimentos apenas com o garfo e picar bem os alimentos mais duros, como carnes, é o bastante. "),
   const CardItem(
       id: "5",
       titulo: "Passo 5",
       info: "Não oferecer açúcar à criança até 2 anos de idade.",
-      image: "assets/imgs/mercury.png",
+      image: "assets/imgs/ps5.png",
       texto:
           "O consumo de açúcar não é necessário e causa danos à saúde como cáries, obesidade ainda na infância e, na vida adulta, pode levar a doenças crônicas, como diabetes. Não inclua na alimentação da criança nem mel, nem açúcar de qualquer tipo (mascavo, demerara, cristal ou refinado [“branco”], rapadura, melaço), nem ofereça preparações ou produtos prontos que contenham algum desses ingredientes (como biscoitos, bolos, iogurtes, entre outros). Os adoçantes (em pó ou líquido) também não devem ser usados na alimentação da  criança até 2 anos, pois são alimentos ultraprocessados e contêm substâncias químicas não adequadas a essa fase da vida."),
   const CardItem(
       id: "6",
       titulo: "Passo 6",
       info: "Não oferecer alimentos ultraprocessados para a criança",
-      image: "assets/imgs/mercury.png",
+      image: "assets/imgs/ps6.png",
       texto:
           "Esses alimentos, geralmente, são pobres em nutrientes e podem conter muito sal, gordura e açúcar, além de aditivos, como adoçantes, corantes e conservantes. O consumo desses alimentos pode levar a problemas como hipertensão, doenças do coração, diabetes, obesidade, cáries e câncer. Fique atenta, pois alguns alimentos ultraprocessados são vistos como alimentos infantis e saudáveis, sendo frequentemente oferecidos às crianças."),
   const CardItem(
     id: "7",
     titulo: "Passo 7",
     info: "Cozinhar a mesma comida para a criança e para família",
-    image: "assets/imgs/mercury.png",
+    image: "assets/imgs/ps7.png",
     texto:
         "Chegada de uma criança é a chance de melhorar a alimentação de toda a família. Preparar a mesma comida para todos, com alimentos in natura ou minimamente processados, sem excesso de gordura, sal e condimentos, agiliza o dia a dia na cozinha e é uma oportunidade de oferecer alimentação adequada e saudável à família e à criança. Planejar a alimentação da semana, organizar as compras para ter os alimentos em casa e cozinhar em maior quantidade para congelar uma parte (para consumir em outros dias) são estratégias que facilitam o cozinhar e garantem comida de verdade todos os dias e em todas as refeições. ",
   ),
@@ -58,7 +58,7 @@ final List<CardItem> passoslist = [
     id: "8",
     titulo: "Passo 8",
     info: "Zelar na hora da alimentação da criança e família",
-    image: "assets/imgs/mercury.png",
+    image: "assets/imgs/ps8.png",
     texto:
         "É fundamental que toda família valorize o momento da alimentação. Comer juntos ajuda a criança a se interessar em experimentar novos alimentos e torna as refeições mais prazerosas. O ambiente acolhedor, tranquilo e a boa relação entre a criança e as pessoas que cuidam dela podem influenciar de forma positiva na aceitação dos alimentos e preparações. Propicie momentos de compartilhamento da refeição em família que envolvam a criança.",
   ),
@@ -66,7 +66,7 @@ final List<CardItem> passoslist = [
     id: "9",
     titulo: "Passo 9",
     info: "Prestar atenção aos sinais de fome e saciedade da criança",
-    image: "assets/imgs/mercury.png",
+    image: "assets/imgs/ps9.png",
     texto:
         "A criança desde cedo é capaz de comunicar quando quer se alimentar ou quando já está satisfeita. Os sinais de fome e saciedade devem ser reconhecidos e respondidos de forma ativa e carinhosa. Alimentar a criança é um processo que demanda paciência e tempo. Estimule a criança a comer, mas sem forçá-la, nem mesmo quando ela estiver doente. O prazer da alimentação está nos sabores, aromas e na forma como a comida é oferecida.",
   ),
@@ -75,7 +75,7 @@ final List<CardItem> passoslist = [
     titulo: "Passo 10",
     info:
         "Cuidar da higiene em todas as etapas da alimentação da criança e da família",
-    image: "assets/imgs/mercury.png",
+    image: "assets/imgs/ps10.png",
     texto:
         "Cuidados com a higiene de quem faz a comida, da cozinha e dos alimentos previnem doenças na criança e na família. Lave as mãos sempre que for cozinhar e alimentar a criança, depois de usar o banheiro, de trocar a fralda e de realizar outras tarefas no cuidado da casa. Quando a criança for comer, também lave as mãos dela.",
   ),
@@ -84,7 +84,7 @@ final List<CardItem> passoslist = [
     titulo: "Passo 11",
     info:
         "Oferecer à criança alimentação adequada e saudável também fora de casa",
-    image: "assets/imgs/mercury.png",
+    image: "assets/imgs/ps11.png",
     texto:
         " É possível manter a alimentação saudável fora de casa. Em passeios, festas e quando for às consultas com a equipe de saúde, continue ofertando os alimentos que a criança come em casa, pois muitos dos alimentos in natura ou minimamente processados (como frutas e legumes crus, frutas secas) podem ficar em temperatura mbiente por algum tempo.",
   ),
@@ -92,7 +92,7 @@ final List<CardItem> passoslist = [
     id: "12",
     titulo: "Passo 12",
     info: "Proteger a criança da publicidade de alimentos",
-    image: "assets/imgs/mercury.png",
+    image: "assets/imgs/ps12.png",
     texto:
         "A criança confunde facilmente a realidade com a ficção dos programas televisivos e da publicidade, porque ela não tem desenvolvida a capacidade de julgamento e decisão. É crucial que ela seja protegida, evitando ao máximo a sua exposição à publicidade. Ela está presente nos programas de TV e nas propagandas nos seus intervalos, na internet, em jogos eletrônicos, entre outros. É um dever de todos proteger a criança da publicidade de alimentos. Crianças menores de 2 anos não devem utilizar televisão, celular, computador e tablet.",
   ),
